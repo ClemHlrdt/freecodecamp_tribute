7c8affb908f8c414791ce4c0a16d575fd0bef52f
ClemHlrdt <clement.helardot@gmail.com>
2017-12-07 20:25:18 +0100

    Added Timeliny plugin
    Plugin added for timeline of Aaron

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
+  $('#example1').timeliny({
+    order: 'asc',
+		className: 'timeliny',
+		wrapper: '<div class="timeliny-wrapper"></div>',
+		boundaries: 2,
+		animationSpeed: 250,
+    hideBlankYears: true,
+		onInit: function() {},
+		onDestroy: function() {},
+		afterLoad: function(currYear) {},
+		onLeave: function(currYear, nextYear) {},
+		afterChange: function(currYear) {},
+		afterResize: function() {}
+  });
 });
